1aa28c2835e00122e69ecff07bffe943fc0c86d1
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2020-12-30 20:26:26 -0600

    attempt bug fix image retrieval

--- client/src/components/post/PostItem.js
+++ client/src/components/post/PostItem.js
@@ -34,9 +34,11 @@ class PostItem extends Component {
   render() {
     const { description, image } = this.props.post;
     const { firstName, lastName, _id } = this.props.post._user;
+    const imageSrc = getS3File(image);
+    console.log(imageSrc);
     return (
       <div className="card">
-        <img className="card-img-top" src={getS3File(image)} alt="Card" />
+        <img className="card-img-top" src={imageSrc} alt="Card" />
         <div className="card-body card-padding">
           <div className="card-text">
             <Link
